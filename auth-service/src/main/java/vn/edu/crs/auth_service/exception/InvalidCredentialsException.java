d6c3d1d0af273632912605e43517de6bad4059e4
QuachVanTu <quachvantuzzz@gmail.com>
2026-08-13 10:19:28 +0700

    init: auth-service with JWT login + seed data

---
+++ auth-service/src/main/java/vn/edu/crs/auth_service/exception/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package vn.edu.crs.auth_service.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
